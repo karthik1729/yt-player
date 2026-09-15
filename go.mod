6f1c3f70901a170768aec167384239ba2f21956f
Karthik Th <karthik@kloudlite.io>
2026-09-15 16:07:19 +0530

    Remove controls subcommand

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/karthik1729/yt-player
 
 go 1.26.4
 
-require (
-	github.com/modelcontextprotocol/go-sdk v1.8.0
-	golang.org/x/sys v0.41.0
-)
+require github.com/modelcontextprotocol/go-sdk v1.8.0
 
 require (
 	github.com/google/jsonschema-go v0.4.3 // indirect
@@ -14,5 +11,6 @@ require (
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	golang.org/x/oauth2 v0.35.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,7 +4,6 @@
 //	yt-player          stdio MCP bridge (starts the daemon if needed)
 //	yt-player daemon   background service owning mpv and the queue
 //	yt-player call <tool> [json-args]   run one tool on the running daemon
-//	yt-player controls [label]   one-line player with clickable ytp:// links, for a terminal pane
 package main
 
 import (
@@ -45,14 +44,6 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		return
-	}
-	if len(os.Args) > 1 && os.Args[1] == "controls" {
-		label := ""
-		if len(os.Args) > 2 {
-			label = os.Args[2]
-		}
-		controls(sock, label)
 		return
 	}
 	if err := bridge(dir, sock); err != nil {
